4fd799418fa97c11e14dc3492d36afb12b2cf296
changhee16 <81596310+changhee16@users.noreply.github.com>
2025-07-25 14:39:47 +0900

    Time: 1 ms (43.31%), Space: 10.8 MB (38.91%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,33 +1,27 @@
 #include <algorithm>
-//1. c++ stl 사용 X버전. iterative 호출 
+//1. c++ stl 사용 X버전. 재귀 호출 버전 (백트래킫)
 class Solution {
 public:
-
-    bool nextPermutation(vector<int>& data)
+    vector <vector <int>> res;
+    void backtracking(vector<int>& nums, int depth)
     {
-	int i = data.size() - 1, j = data.size() - 1;
- 
-	/* 1번 : 꼭대기 찾기 */
-	while (i > 0 && data[i - 1] >= data[i]) i--;
-	if (i == 0) return false;
- 
-	/* 2번 j값 찾기 */
-	while (data[i - 1] >= data[j]) j--;
-	swap(data[i - 1], data[j]);		// 3번 스왑하기
- 
-	/* 4번 나머지 정렬하기 */
-	j = data.size() - 1;
-	for (; i < j; ++i, --j)
-		swap(data[i], data[j]);
-	return true;
+        if (depth == nums.size())
+        {
+            //cout << nums << "\n";
+            res.push_back(nums);
+            return ;
+        }
+        for (int i = depth; i < nums.size(); i++) //이부분이 핵심 
+        {
+            swap(nums[depth], nums[i]);
+            backtracking(nums, depth+1);
+            swap(nums[depth], nums[i]); 
+        }
     }
 
     vector<vector<int>> permute(vector<int>& nums) {
-        vector <vector <int>> res;
-        sort(nums.begin(), nums.end()); //오름차순으로 정렬
-        res.push_back(nums);
-        while (nextPermutation(nums))
-            res.push_back(nums);
+        
+        backtracking(nums, 0);
         return res;
     }
 };
